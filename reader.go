26b5050694dbf5aad90b39afc2121b0a4d6b4745
Timothy Chen <tnachen@gmail.com>
2018-01-08 20:23:52 +0000

    Update code to support arbitrary label

--- reader.go
+++ reader.go
@@ -9,6 +9,28 @@ import (
 	"github.com/prometheus/tsdb"
 )
 
+func PrintLabelValues(db *tsdb.DB, metricName string) {
+	q, err := db.Querier(math.MinInt64, math.MaxInt64)
+	if err != nil {
+		fmt.Println("Unable to create queries: " + err.Error())
+		return
+	}
+	defer q.Close()
+
+	vals, err := q.LabelValues(metricName)
+	if err != nil {
+		fmt.Println("Unable to get label values: " + err.Error())
+		return
+	}
+
+	for _, val := range vals {
+		// We don't need solr data and there are tons of them.
+		if !strings.HasPrefix(val, "solr") {
+			fmt.Println(val)
+		}
+	}
+}
+
 func main() {
 	path := os.Args[1]
 	db, err := tsdb.Open(path, nil, nil, nil)
@@ -17,22 +39,10 @@ func main() {
 		return
 	}
 
-	q, err := db.Querier(math.MinInt64, math.MaxInt64)
-	if err != nil {
-		fmt.Println("Unable to create queries: " + err.Error())
-		return
-	}
-	defer q.Close()
-
-	vals, err := q.LabelValues("__name__")
-	if err != nil {
-		fmt.Println("Unable to get label values: " + err.Error())
-		return
+	labelName := "__name__"
+	if len(os.Args) >= 3 {
+		labelName = os.Args[2]
 	}
 
-	for _, val := range vals {
-		if !strings.HasPrefix(val, "solr") {
-			fmt.Println(val)
-		}
-	}
+	PrintLabelValues(db, labelName)
 }
