e51fb4f8c9b277f817d93117859fa8f91c838bb0
Xiaoyun Zhu <xiaoyzhu@yahoo.com>
2018-01-26 00:21:35 -0800

    combine cpu and memory in waste calculation

--- stats_aggregator/stats_aggregator.py
+++ stats_aggregator/stats_aggregator.py
@@ -10,8 +10,8 @@ METRIC_LIST = ['_util_per_instance_95p', '_util_per_instance_max', '_util_per_po
 COST_MAP = {'action-classify': 0.248, 'action-gke': 1.22, 'db': 0.663, 'db-preempt': 0.663, 'druid-preempt': 0.663,
             'druid-ssd-preempt': 0.704, 'mixed': 0.248, 'mixed-preempt': 0.248, 'nginx': 0.266, 'ping-gke': 0.69}
 PERCENTILES = [.5, .95, .99]
-#END_TIME = 1514995200917
-END_TIME = 1515028900917
+END_TIME = 1514995200917
+#END_TIME = 1515028900917
 
 class StatsAggregator(object):
     def __init__(self, metric_name):
@@ -50,9 +50,10 @@ class StatsAggregator(object):
 
         plt.close('all')
 
-    def compute_waste(self, res, csvfile1, csvfile2, out_dir):
-        df_util = pd.read_csv(csvfile1, sep=',')
-        df_num = pd.read_csv(csvfile2, sep=',')
+
+    def compute_waste_res(self, res, csv_util, csv_num, out_dir):
+        df_util = pd.read_csv(csv_util, sep=',')
+        df_num = pd.read_csv(csv_num, sep=',')
         waste_list = []
 
         for nodepool in df_util['node_pool'].unique():
@@ -71,7 +72,44 @@ class StatsAggregator(object):
                                'unused instances': waste_num, 'wasted cost': waste_cost})
             print("Average hourly cost wasted for %s resource in nodepool %s: %.2f" %(res, nodepool, waste_cost))
 
-        outfile = out_dir + res + "_waste_cost.csv"
+        outfile = out_dir + res + '_waste_cost' + ".csv"
+        waste_df = pd.DataFrame(waste_list)
+        waste_df.to_csv(outfile)
+
+
+    def compute_waste_mixed(self, res_list, csv_list, csv_num, out_dir):
+        if len(res_list) > 2:
+            print("Cannot combine more than two resources!")
+            return
+
+        df_util1 = pd.read_csv(csv_list[res_list[0]], sep=',')
+        df_util2 = pd.read_csv(csv_list[res_list[1]], sep=',')
+        df_num = pd.read_csv(csv_num, sep=',')
+        waste_list = []
+
+        for nodepool in COST_MAP.keys():
+            util1_pool = df_util1.loc[(df_util1['node_pool'] == nodepool) & (df_util1['time'] <= END_TIME)][['time', 'value']]
+            util2_pool = df_util2.loc[(df_util2['node_pool'] == nodepool) & (df_util2['time'] <= END_TIME)][['time', 'value']]
+            util1_pool['time'] = (util1_pool['time'] / 1000).astype('int64')
+            util2_pool['time'] = (util2_pool['time'] / 1000).astype('int64')
+            df_mixed = util1_pool.set_index('time').join(util2_pool.set_index('time'), how='outer',
+                                                         lsuffix=res_list[0], rsuffix=res_list[1]).fillna(0)
+            df_mixed['max_util'] = df_mixed[['value'+res_list[0], 'value'+res_list[1]]].max(axis=1)
+
+            num_pool = df_num.loc[(df_num['node_pool'] == nodepool) & (df_num['time'] <= END_TIME)][['time', 'value']]
+            num_avg = num_pool.value.mean()
+            print("Average provisioned instances for nodepool %s: %.1f" %(nodepool, num_avg))
+            num_pool['time'] = (num_pool['time'] / 1000).astype('int64')
+
+            df_joined = df_mixed.join(num_pool.set_index('time'), how='inner',
+                                                         lsuffix='_util', rsuffix='_num')
+            waste_num = ( (1 - df_joined.max_util/100) * df_joined.value ).mean()
+            waste_cost = waste_num * COST_MAP[nodepool]
+            waste_list.append({'node pool': nodepool, 'live instances': num_avg,
+                               'unused instances': waste_num, 'wasted cost': waste_cost})
+            print("Average hourly cost wasted in nodepool %s: %.2f" %(nodepool, waste_cost))
+
+        outfile = out_dir + 'waste_cost_mixed' + ".csv"
         waste_df = pd.DataFrame(waste_list)
         waste_df.to_csv(outfile)
 
@@ -85,6 +123,11 @@ if __name__ == "__main__":
 
     aggregator = StatsAggregator("_util_per_pool")
     csv_list = aggregator.get_csv_list(RES_LIST, INPUT_DIR)
-    csv2 = INPUT_DIR + "num_instances_per_pool.csv"
-    for res, csv1 in csv_list.items():
-        aggregator.compute_waste(res, csv1, csv2, OUTPUT_DIR)
+    csv_num = INPUT_DIR + "num_instances_per_pool.csv"
+    for res, csv_res in csv_list.items():
+        aggregator.compute_waste_res(res, csv_res, csv2, OUTPUT_DIR)
+
+    aggregator = StatsAggregator("_util_per_pool")
+    csv_list = aggregator.get_csv_list(RES_LIST, INPUT_DIR)
+    csv_num = INPUT_DIR + "num_instances_per_pool.csv"
+    aggregator.compute_waste_mixed(RES_LIST, csv_list, csv_num, OUTPUT_DIR)
